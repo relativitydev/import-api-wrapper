ee06c832ce55b89b74ef7fa97509e7a9f9b2d51b
Joe Henderson Staudt <joe.hendersonstaudt@relativity.com>
2019-10-31 18:20:02 -0500

    Initial Commit

---
+++ ImportAPI-Plugin/Relativity.DataExchange.Wrapper.Core/ImportProgressEventArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace Relativity.DataExchange.Wrapper
+{
+	using System;
+
+	public sealed class ImportProgressEventArgs : EventArgs
+	{
+		public ImportProgressEventArgs(long row)
+		{
+			this.Row = row;
+		}
+
+		public long Row
+		{
+			get;
+		}
+	}
+}
